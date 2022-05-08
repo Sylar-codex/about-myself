fd94bcd82ba84cf702bb04d73a23bc97072f22d6
Sylar <user@users-MacBook-Pro.local>
2022-05-08 13:10:19 +0100

    changed filexxx

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ function Toggle() {
     document.getElementById('nav-links').classList.toggle("show");
 }
 document.getElementById('icon').addEventListener('click', Toggle);
+document.getElementById('nav-home').addEventListener('click', Toggle);
+document.getElementById('nav-about').addEventListener('click', Toggle);
+document.getElementById('nav-projects').addEventListener('click', Toggle);
+document.getElementById('nav-contact').addEventListener('click', Toggle);
 
